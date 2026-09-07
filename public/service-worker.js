6400164968ebcfad6ede4535333db7428bfb13d9
Joshua Werlein <jjwerlein@gmail.com>
2026-09-07 17:52:26 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,3 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
